25cb1eb52fa5211cdb17a9f4f52a2e92b14cfa33
Dan F-M <danfm@nyu.edu>
2015-03-27 23:39:02 -0400

    proper pool

--- peerless/pool.py
+++ peerless/pool.py
@@ -17,54 +17,31 @@ class IPythonPool(object):
         self.client = Client(**kwargs)
 
     def run(self, function, arglist, dirlist, quiet=False, **kwargs):
-        # Get a view into the pool.
-        view = self.client[:]
-
-        # Loop over the argument list and execute the jobs.
-        jobs = []
-        for args, bp in zip(arglist, dirlist):
-            # Make sure that the path exists.
-            if not os.path.exists(bp):
-                os.makedirs(bp)
-
-            # Build the function wrapper.
-            f = _wrapper(function, bp, **kwargs)
-
-            # Apply the job on the client.
-            jobs.append((bp, view.apply(f, args)))
-
-        # Monitor the jobs and check for completion and errors.
-        retrieved = [False for i in range(len(jobs))]
-        while not all(retrieved):
-            for i, (bp, j) in enumerate(jobs):
-                if j.ready() and not retrieved[i]:
-                    try:
-                        j.get()
-                    except Exception:
-                        if not quiet:
-                            raise
-                    retrieved[i] = True
-            time.sleep(3)
+        f = _wrapper(function, **kwargs)
+        return self.client[:].map(f, zip(dirlist, arglist))
 
 
 class _wrapper(object):
 
-    def __init__(self, function, bp, name="output.log", error="error.log",
+    def __init__(self, function, name="output.log", error="error.log",
                  **kwargs):
-        self.filename = os.path.join(bp, name)
-        self.error = os.path.join(bp, error)
+        self.filename = name
+        self.error = error
         self.function = function
         self.kwargs = kwargs
 
     def __call__(self, args):
+        bp = args[0]
+        if not os.path.exists(bp):
+            os.makedirs(bp)
         try:
             strt = time.time()
-            with Capturing(self.filename):
-                result = self.function(args, **(self.kwargs))
+            with Capturing(os.path.join(bp, self.filename)):
+                result = self.function(args[1], **(self.kwargs))
                 print("Execution time: {0} seconds".format(time.time()-strt))
             return result
         except Exception:
-            with open(self.error, "a") as f:
+            with open(os.path.join(bp, self.error), "a") as f:
                 f.write(traceback.format_exc() + "\n\n")
             raise
 

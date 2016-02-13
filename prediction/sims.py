1448b503132383dcce223cb1eddef5d0540590f1
Timothy Morton <tim.morton@gmail.com>
2016-02-13 14:25:23 -0500

    EB simulations done

--- prediction/sims.py
+++ prediction/sims.py
@@ -6,7 +6,7 @@ from isochrones.dartmouth import Dartmouth_Isochrone
 from vespa.stars.utils import draw_eccs # this is a function that returns
                                         # empirically reasonably eccentricities
                                         # for given binary periods.
-from scipy.stats import norm 
+from scipy.stats import norm, binom
 from vespa.transit_basic import _quadratic_ld
 
 from vespa.stars.utils import G, MSUN, RSUN, AU, DAY
@@ -64,6 +64,9 @@ class BinaryPopulation(object):
         self._ic = ic
         self._params = params
 
+        for k,v in self.prop_columns.items():
+            self.stars.loc[:, k] = self.stars.loc[:, v]
+
     @property
     def params(self):
         if self._params is not None:
@@ -82,8 +85,8 @@ class BinaryPopulation(object):
         return len(self.stars)
 
     def __getattr__(self, name):
-        if name in self.prop_columns:
-            return self.stars[self.prop_columns[name]].values
+        if name in self.stars:
+            return self.stars[name].values
         elif name in self.physical_props:
             if name not in self.stars:
                 self._generate_binaries()
@@ -141,7 +144,6 @@ class BinaryPopulation(object):
         _, _, _, mu_logp, sig_logp = self.params
 
         N = self.N
-        stars = self.stars.copy()
 
         period = 10**(norm(np.log10(mu_logp), sig_logp).rvs(N)) * 365.25
         ecc = draw_eccs(N, period)
@@ -163,32 +165,74 @@ class BinaryPopulation(object):
         k = self.radius_B / self.radius_A
         T14_pri = period/np.pi*np.arcsin(self.radius_A*RSUN/a * np.sqrt((1+k)**2 - b_pri**2)/np.sin(inc)) *\
             np.sqrt(1-ecc**2)/(1+ecc*np.sin(w))
-        T14_sec = period/np.pi*np.arcsin(self.radius_A*RSUN/a * np.sqrt((1+k)**2 - b_pri**2)/np.sin(inc)) *\
+        T14_sec = period/np.pi*np.arcsin(self.radius_A*RSUN/a * np.sqrt((1+k)**2 - b_sec**2)/np.sin(inc)) *\
             np.sqrt(1-ecc**2)/(1-ecc*np.sin(w))
+    
+        T14_pri[np.isnan(T14_pri)] = 0.
+        T14_sec[np.isnan(T14_sec)] = 0.
+
         for i in xrange(N):
             if tra[i]:
-                d_pri[i] = _quadratic_ld._quadratic_ld(np.array([b_pri[i]]), k[i], 0.394, 0.296, 1)[0]
+                f = _quadratic_ld._quadratic_ld(np.array([b_pri[i]]), k[i], 0.394, 0.296, 1)[0]
+                F2 = self.flux_ratio[i]
+                d_pri[i] = 1 - (F2 + f)/(1+F2)
             if occ[i]:
-                d_sec[i] = _quadratic_ld._quadratic_ld(np.array([b_sec[i]/k[i]]), 1./k[i], 0.394, 0.296, 1)[0]
+                f = _quadratic_ld._quadratic_ld(np.array([b_sec[i]/k[i]]), 1./k[i], 0.394, 0.296, 1)[0]
+                F2 = self.flux_ratio[i]
+                d_sec[i] = 1 - (1 + F2*f)/(1+F2)
 
+        stars = self.stars.copy()
         for var in self.orbital_props:
             stars.loc[:, var] = eval(var)
 
         self.stars = stars
 
-
-    def observe(self, duration=4):
+    def observe(self, query=None):
         """
-        duration in years
-
         Returns catalog of the following observable quantities:
           
-          * n_signals
-          * period
+          * n_pri
+          * n_sec
           * d_pri
           * d_sec
           * T14_pri
           * T14_sec
+       
+        assumes stars dataframe has 'dataspan' and 'dutycycle' columns
         """
         
+        # Select only systems with eclipsing (or occulting) geometry
+        m = self.tra | self.occ
+        if query is None:
+            query = 'dataspan > 0'
+        df = self.stars.loc[m].query(query).copy()
+
+        # Phase of secondary (Hilditch (2001) , Kopal (1959))
+        #  Primary is at phase=0
+        X = np.pi + 2*np.arctan(df.ecc * np.cos(df.w) / np.sqrt(1-df.ecc**2))
+        secondary_phase = X - np.sin(X)
+
+        # Assign each system a random phase at t=0;
+        N = len(df)
+        initial_phase = np.random.random(N)
+        final_phase = initial_phase + df.dataspan/df.period
+
+        # Determine number of primary & secondary eclipses, assuming perfect duty cycle
+        n_pri_ideal = np.floor(final_phase) * df.tra
+        n_sec_ideal = (np.floor(final_phase + secondary_phase) - 
+                       np.floor(initial_phase + secondary_phase))*df.occ
+
+        # Correct for duty cycle.  
+        # Each event has probability (1-dutycycle) of landing in a gap.
+        n_pri = np.zeros(N)
+        n_sec = np.zeros(N)
+        for i, (n1,n2,d) in enumerate(zip(n_pri_ideal,
+                                          n_sec_ideal,
+                                          df.dutycycle)):
+            n_pri[i] = binom(n1,d).rvs()
+            n_sec[i] = binom(n2,d).rvs()
         
+        df.loc[:, 'n_pri'] = n_pri
+        df.loc[:, 'n_sec'] = n_sec
+
+        return df.query('(n_pri > 0) or (n_sec > 0)')

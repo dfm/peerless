350d46956c87214c420541f18099ba7935044855
Eric Agol <agol@uw.edu>
2016-07-22 11:28:01 -0400

    overplotted completeness with period histogram

--- document/completeness.jl
+++ document/completeness.jl
@@ -1,15 +1,17 @@
 using PyPlot
 using CGS
 nperiod = 100
-p = logspace(log10(2*365.25),log10(5000),nperiod)
-lnR = log(5*REARTH/RSUN)
+p = logspace(log10(2),log10(5000/365.25),nperiod)
+lnR = log(4*REARTH/RJUPITER)
 qdet = zeros(nperiod)
 for i=1:nperiod
-  qdet[i] = minimum([maximum([(-0.06*log(p[i])+0.78)*(-0.45*lnR+0.95),0]),1])/(1+exp(-(0.7*log(p[i])-1.1)*(lnR-(-0.07*log(p[i])-2.8))))
+  qdet[i] = minimum([maximum([(-0.13*log(p[i])+0.95)*(-0.20*lnR+0.90),0]),1])/(1+exp(-(0.7*log(p[i])+3.06)*(lnR-(-0.07*log(p[i])-0.91))))
 end
 fduty = 0.97
 qwin = 1. - (1.-fduty).^(4./p)
-qgeom = 0.005./(p./365.25).^(2/3)
-loglog(p,qdet)
-loglog(p,qdet.*qwin)
-loglog(p,qdet.*qwin.*qgeom)
+qgeom = 0.005./p.^(2/3)
+#loglog(p,qdet)
+#loglog(p,qdet.*qwin)
+semilogx(p*365.25,qdet.*qwin.*qgeom.*1e4)
+plot([200,200,340,340,590,590],[0,15,15,12,12,0])
+plot([590,590,1000,1000,1700,1700,3000,3000,5000,5000],[0,1,1,6,6,5,5,3,3,0])
